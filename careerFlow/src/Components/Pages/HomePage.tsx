fbaf6de6e088bb9668afae2bf7d241c3cc22cdbd
TheMikeKaisen <h.karthiknair@gmail.com>
2024-09-21 13:42:44 +0530

    designing landing page

--- careerFlow/src/Components/Pages/HomePage.tsx
+++ careerFlow/src/Components/Pages/HomePage.tsx
@@ -1,10 +1,13 @@
 import Header from '../Header/Header'
+import DreamJobs from './LandingPage/DreamJobs'
 
 const HomePage = () => {
   return (
     <div className='min-h-[100vh] bg-mine-shaft-950 font-["poppins"]'>
 
     <Header />
+    <DreamJobs />
+    
     </div>
   )
 }
