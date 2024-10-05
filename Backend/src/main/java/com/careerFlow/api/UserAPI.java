286ce97d088c8a8c713c4dcf828220f774e1672c
TheMikeKaisen <h.karthiknair@gmail.com>
2024-10-05 11:35:21 +0530

    add validations to register api

--- Backend/src/main/java/com/careerFlow/api/UserAPI.java
+++ Backend/src/main/java/com/careerFlow/api/UserAPI.java
@@ -1,7 +1,9 @@
 package com.careerFlow.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +14,20 @@ import com.careerFlow.dto.UserDTO;
 import com.careerFlow.entity.User;
 import com.careerFlow.service.UserService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @CrossOrigin
+@Validated
 @RequestMapping("/users")
 public class UserAPI {
+	
+	@Autowired
 	private UserService userService;
 	
+	
 	@PostMapping("/register")
-	public ResponseEntity<UserDTO>registerUser(@RequestBody UserDTO userDTO){
+	public ResponseEntity<UserDTO>registerUser(@RequestBody @Valid UserDTO userDTO){
 		userDTO = userService.registerUser(userDTO);
 		return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
 	}
--- Backend/src/main/java/com/careerFlow/dto/UserDTO.java
+++ Backend/src/main/java/com/careerFlow/dto/UserDTO.java
@@ -2,6 +2,10 @@ package com.careerFlow.dto;
 
 import com.careerFlow.entity.User;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +16,18 @@ import lombok.NoArgsConstructor;
 public class UserDTO {
 	
 	private String id;
+	
+	@NotBlank(message="{user.name.absent}")
 	private String name;
 	
-	
+	@NotNull(message="{user.email.absent}")
+	@Email(message="{user.email.invalid}")
 	private String email;
+	
+	@NotNull(message="{user.password.absent")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$", message="{user.password.invalid}")
 	private String password;
+	
 	private AccountType accountType;
 	
 	public User toEntity() {
